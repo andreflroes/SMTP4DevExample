a15ab36109cc638a186d381705feb9af6208e495
André Luiz Flôres <flores.luiz.andre@gmail.com>
2022-11-05 18:04:11 -0300

    Exemplo de e-mail utilizando a bibilioteca SMTP4Dev

---
+++ SMTP4Dev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+var emailMessage = new MimeMessage();
+emailMessage.From.Add(new MailboxAddress("System", "system@test.com"));
+emailMessage.To.Add(new MailboxAddress("André Luiz", "andre@gmail.com"));
+emailMessage.Subject = "This is a test";
+var bodyBuilder = new BodyBuilder();
+bodyBuilder.HtmlBody = "<h1>Important message</h1><p>This is from the system</p>";
+bodyBuilder.Attachments.Add("C:/Users/Pichau/Desktop/Faculdade/MapaLogicaParaComputacao.pdf");
+emailMessage.Body = bodyBuilder.ToMessageBody();
+
+using (var client = new SmtpClient())
+{
+    var secureSocketOptions = SecureSocketOptions.None;
+    client.Connect("localhost", 25, secureSocketOptions);
+    await client.SendAsync(emailMessage);
+    client.Disconnect(true);
+    Console.WriteLine("Sucesso ao enviar o e-mail");
+}
